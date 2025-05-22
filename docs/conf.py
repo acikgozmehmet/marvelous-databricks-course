4164da44cc2d196debd58bf4f194917bd7b51f1b
Mehmet Acikgoz <acikgozmm@gmail.com>
2025-05-22 15:27:50 -0500

    update the page content

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,8 @@
 import sphinx_rtd_theme  # noqa
 import os
 import sys
-[sys.path.insert(0, x[0]) for x in os.walk('../src')]
+
+[sys.path.insert(0, x[0]) for x in os.walk("../src")]
 
 # Configuration file for the Sphinx documentation builder.
 #
@@ -11,31 +12,30 @@ import sys
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'house_prices'
-copyright = '2025, MLLAB'
-author = 'MLLAB'
+project = "house_prices"
+copyright = "2025, Marvelous MLOps"
+author = "Mehmet Acikgoz"
 
-version = '0.1.1'
-release = '0.1.1'
+version = "0.1.1"
+release = "0.1.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     "logo only": True,
     "collapse_navigation": True,
@@ -45,8 +45,7 @@ html_theme_options = {
     "titles_only": False,
 }
 
-html_static_path = ['_static']
-
+html_static_path = ["_static"]
 
 
 def setup(app):
